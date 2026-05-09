7e0b11472d86fa31f36ee160a1316b2970cf9a0f
new6ie <130747953+new6ie3@users.noreply.github.com>
2026-05-09 14:22:29 +0900

    Add files via upload

---
+++ Lv08_While_break/Lv8_5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[6] = { 3, 4, 1, 6, 7, 5 };
+	int index = 0;
+
+	while (index < 6)
+	{
+		cout << arr[index] << " ";
+		index++;
+	}
+
+	return 0;
+}
